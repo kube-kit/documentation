d82c4a9ec763f3d646e2ab0446a3eee9fa7e5dea
FarhanTausif <farhantausif4@gmail.com>
2025-12-17 13:10:54 +0600

    refactor: remove control-k.js and associated CSS for search shortcut

--- _plugins/gitbook-plugin-kubekit-custom/index.js
+++ _plugins/gitbook-plugin-kubekit-custom/index.js
@@ -3,7 +3,6 @@ module.exports = {
         assets: './assets',
         js: [
             'clear-btn.js',
-            'control-k.js',
             'redirect-marketing-site.js',
             'footer.js',
         ]
